80061005bedf87f86ba0425a44a3fa939f4a0e91
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-27 19:45:13 -0300

    Finalizado!

--- config/script.js
+++ config/script.js
@@ -1,5 +1,8 @@
-var filmes = ["DIA DO SIM", "HOMEM ARANHA - De volta ao lar", "VINGADORES - Guerra Infinita"]
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BNTMwMzA4YzYtMzkwNC00N2ZhLTk5MjQtMjg3ODA3NDY3ZmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg"]
 
-for (var i = 0; i <= 0; i--) {
-    console.log(filmes[i])
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">")
 }
+
+//i++
+//i--
