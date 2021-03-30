d47a424122c11e857764e365f86ed7d4c79b1388
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-29 22:51:21 -0300

    Re-masterizado

--- config/script.js
+++ config/script.js
@@ -1,8 +1,27 @@
-var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BNTMwMzA4YzYtMzkwNC00N2ZhLTk5MjQtMjg3ODA3NDY3ZmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+function adicionarFilmeFavorito() {
+    var campoFilmeFavorito = document.querySelector('#filme')
 
-for (var i = 0; i < listaFilmes.length; i++) {
-    document.write("<img src=" + listaFilmes[i] + ">")
+    var filmeFavorito = campoFilmeFavorito.value
+
+    //Validação
+    if (filmeFavorito.endsWith('.jpg')){
+
+        listarFilmesNaTela(filmeFavorito)
+
+    } else {
+        alert("Imagem Invalida! Impossivel adicionar")
+    } 
+    //Excluir texto
+    campoFilmeFavorito.value = ""
+    
 }
 
-//i++
-//i--
+function listarFilmesNaTela(filme) {
+    var listaFilmes = document.querySelector('#listaFilmes')
+
+    var elementoFilme = `<img src= ${filme}>`
+
+    listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+
+}
+
